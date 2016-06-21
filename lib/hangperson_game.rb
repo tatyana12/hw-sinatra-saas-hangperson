e4da2f17c6ca0c824a3e5a0207fad11fcff1817c
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2016-06-21 20:30:46 +0000

    Done HW2 part1

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,7 +10,11 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+  
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def self.get_random_word
     require 'uri'
@@ -18,5 +22,34 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+  
+  def guess letter
+    raise ArgumentError unless letter =~ /[A-Za-z]/
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter) || ('A'..'Z').include?(letter)
+    if @word.include? letter
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+  end
+  
+  def check_win_or_lose
+    if @word == word_with_guesses
+      :win
+    elsif @wrong_guesses.size == 7
+      :lose
+    else
+      :play
+    end
+  end
 
+  def word_with_guesses
+    @word.split('').each_with_object('') do |letter, output|
+      if @guesses.include?(letter)
+        output << letter
+      else
+        output << '-'
+      end
+    end
+  end
 end
